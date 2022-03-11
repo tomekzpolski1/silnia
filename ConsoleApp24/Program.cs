6b6b1039c290fe76cc672258db5ef2161f8e3480
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-11 18:53:29 +0100

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n");
+            double n = double.Parse(Console.ReadLine());
+            double wynik = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                wynik = wynik * i;
+            }
+            Console.WriteLine("{0}! wynosi {1} ", n, wynik);
+        }
+    }
+}
